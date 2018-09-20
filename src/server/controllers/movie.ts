b7aaf7d4fe2003ff40605a85d160d7cb1a039d9d
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-09-21 01:50:15 +0200

    async await movieControllers

--- src/server/controllers/movie.ts
+++ src/server/controllers/movie.ts
@@ -3,7 +3,9 @@ import axios, { AxiosResponse } from 'axios';
 
 import { Request, Response } from 'express';
 
-const Movie:any = mongoose.model('movies');
+const Movie = mongoose.model('movies');
+
+import { prepareDataToSave  } from '../helpers';
 
 const baseUrl: string = 'http://www.omdbapi.com';
 const apiKey: string = '?apikey=e01a9718';
@@ -11,45 +13,70 @@ const apiKey: string = '?apikey=e01a9718';
 
 export class MovieController{
 
-    public getMovies = (req: Request, res: Response) => {
+    public getMovies = async (req: Request, res: Response) => {
 
-      Movie
-        .find({})
-        .then((movies: object[]) => {
-            res.status(200).json({ success: true, data: movies, message: 'All movies successfully fetched.' });
-        })
+      const movies: object[] = await Movie.find({});
+
+      return res.status(200).json({ success: true, data: movies, message: 'All movies successfully fetched.' });
     }
 
     public addMovie = async (req: Request, res: Response) => {
 
       if(!req.body.title) {
-          return res.status(400).json({ success: false, data: null, message: 'Request body should contain movie title' });
+         return res
+                  .status(400)
+                  .json({
+                    success: false,
+                    data: null,
+                    message: 'Request body should contain movie title'
+                  });
+     }
+
+     if(req.body.title.length < 3) {
+        return res
+                 .status(400)
+                 .json({
+                   success: false,
+                   data: null,
+                   message: 'Title should be at least 3 characters long.'
+                 });
+    }
+
+     const resp: AxiosResponse = await axios.get(`${baseUrl}/${apiKey}&type=movie&t=${req.body.title.replace(' ', '_')}`);
+
+     if(resp.data.Response === 'False') {
+         return res
+                 .status(404)
+                 .json({
+                   success: false,
+                   data: null,
+                   message: 'Movie with provided title does not exist.'
+                 });
+     }
+
+      const movie: object = await Movie.findOne({ imdbID: resp.data.imdbID });
+
+      if(movie) {
+          return res
+                  .status(400)
+                  .json({
+                    success: false ,
+                    data: movie,
+                    message: 'Movie with provided title already exists in application db'
+                  })
       }
 
-      const title: RegExp = new RegExp(["^", req.body.title, "$"].join(""), "i");
+      const { response, ...movieData } = prepareDataToSave(resp.data);
+      const createdMovie = await Movie.create(movieData);
 
-      return axios
-        .get(`${baseUrl}/${apiKey}&type=movie&t=${req.body.title.replace(' ', '_')}`)
-        .then((response: AxiosResponse) => {
-          if(response.data.Response === 'False') {
-             return res
-                     .status(404)
-                     .json({ success: false, data: null, message: 'Movie with provided title does not exist in both application db and external API' });
-          }
+      return res
+              .status(201)
+              .json({
+                success: true,
+                data: createdMovie ,
+                message: 'Movie successfully fetched from external API and added to application db'
+              });
 
-          return Movie.findOrCreate({ imdbID: response.data.imdbID })
-
-
-        })
-        .then(movie => {
-          res
-            .status(201)
-            .json({
-              success: true,
-              data: movie,
-              message: 'Movie successfully fetched from external API and added to application db'
-            });
-        })
     }
 
 
---
+++ src/server/helpers/prepareDataToSave.ts
@@ -0,0 +1,18 @@
+export const prepareDataToSave = (input: object) => {
+    if (typeof input !== 'object') return input;
+    if (Array.isArray(input)) return input.map(prepareDataToSave);
+    return Object.keys(input).reduce((newObj:object, key:string) => {
+        const val = input[key];
+        const newVal = (typeof val === 'object') ? prepareDataToSave(val) : val;
+        const newKey = key !== 'DVD' ? key.charAt(0).toLowerCase() + key.slice(1) : key.toLowerCase();
+        newObj[newKey] = newVal;
+
+        if(['runtime','year','imdbVotes', 'imdbRating'].includes(newKey)) newObj[newKey] = Number(newObj[newKey]) || 0;
+
+
+        const props = ['director', 'writer', 'actors', 'genre', 'country', 'language'];
+        if(props.includes(newKey)) newObj[newKey] = newObj[newKey].split(',').map(v => v.trim());
+
+        return newObj;
+    }, {});
+};
