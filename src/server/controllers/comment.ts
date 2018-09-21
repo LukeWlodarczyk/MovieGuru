aa6c244803681a8f20487a26bf08fc777e00721f
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-09-21 02:16:09 +0200

    getComment controller with movieId filter

--- src/server/controllers/comment.ts
+++ src/server/controllers/comment.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import * as mongoose from 'mongoose';
 import axios from 'axios';
 
 import CommentSchema from '../models/Comment';
@@ -9,7 +9,24 @@ const Comment = mongoose.model('comments');
 
 export class CommentController{
 
-    public getComments = async (req: Request, res: Response) => {}
+    public getComments = async (req: Request, res: Response) => {
+      const opts: object = req.query;
+
+      for(const key in opts) {
+           if (key !== 'movieId') opts[key] = undefined;
+       }
+
+      const comments = await Comment.find(opts);
+
+      return res
+              .status(200)
+              .json({
+                success: true,
+                data: comments,
+                message: 'Comments successfully fetched.'
+              });
+
+    }
 
     public addComment =  async (req: Request, res: Response) => {}
 
