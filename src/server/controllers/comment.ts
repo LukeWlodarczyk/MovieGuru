d6ede094aa2308a06753ebf5363642c97c42d524
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-09-21 02:22:40 +0200

    addComment controller

--- src/server/controllers/comment.ts
+++ src/server/controllers/comment.ts
@@ -5,12 +5,16 @@ import CommentSchema from '../models/Comment';
 import { Request, Response } from 'express';
 
 const Comment = mongoose.model('comments');
+const Movie = mongoose.model('movies');
 
+interface Opts {
+  id?: string
+}
 
 export class CommentController{
 
     public getComments = async (req: Request, res: Response) => {
-      const opts: object = req.query;
+      const opts: Opts = req.query;
 
       for(const key in opts) {
            if (key !== 'movieId') opts[key] = undefined;
@@ -28,7 +32,57 @@ export class CommentController{
 
     }
 
-    public addComment =  async (req: Request, res: Response) => {}
+    public addComment =  async (req: Request, res: Response) => {
+
+      if(!req.body.id) {
+          return res
+                  .status(400)
+                  .json({
+                    success: false,
+                    data: null,
+                    message: 'Request body should contain movie id.'
+                  });
+      }
+
+      const id: string = req.body.id;
+
+      const isValidId = mongoose.Types.ObjectId.isValid(id);
+
+      if(!isValidId) {
+          return res
+                  .status(400)
+                  .json({
+                    success: false,
+                    data: null,
+                    message: 'Provided id is not valid.'
+                  });
+      }
+
+      const movie = await Movie.findById(id);
+
+      if(!movie) {
+          return res
+                  .status(404)
+                  .json({
+                    success: false,
+                    data: null,
+                    message: 'Movie with provided id does not exist. You cannot add comment to nonexistent movie.'
+                  });
+      }
+
+      const comment = await Comment.create({
+          text: req.body.text,
+          movieId: id,
+      });
+
+      return res
+              .status(201)
+              .json({
+                success: true,
+                data: comment,
+                message: 'Comment successfully created.'
+              });
+    }
 
 
 }
