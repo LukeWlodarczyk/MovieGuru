7972afae759f05f2229e290e7f891534dcd314e9
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-09-25 17:36:15 +0200

    correct typo

--- src/universal/components/MovieCard/index.tsx
+++ src/universal/components/MovieCard/index.tsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 import { IMovie } from '../../models';
 
-import { Card } from './style';
+import { Card } from './styled';
 
 const MovieCard: SFC<IMovie> = ({ title, plot, imdbID, _id, idx }) => (
   <Link to={`/movies/${_id}/${idx}`}>
-    <p>{title}</p>
-    <p>{plot}</p>
+    <Card>
+      <p>{title}</p>
+      <p>{plot}</p>
+    </Card>
   </Link>
 );
 
