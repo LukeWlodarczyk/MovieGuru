f411d38dd4977cb719dd13dfd1e31fffe3dbbc4a
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-10-11 00:14:18 +0200

    add User static methods and ts types

--- src/server/models/User.ts
+++ src/server/models/User.ts
@@ -1,6 +1,21 @@
 import * as mongoose from "mongoose";
+import * as bcrypt from "bcryptjs";
 
-const UserSchema = new mongoose.Schema({
+import { Document, Schema, Model } from "mongoose";
+
+export interface IUserDocument extends Document {
+  email: string;
+  username: string;
+  password: string;
+}
+
+export interface IUser extends IUserDocument {
+  comparePassword(password: string): boolean;
+}
+
+export interface IUserModel extends Model<IUser> {}
+
+const UserSchema: Schema = new mongoose.Schema({
   username: {
     type: String,
     required: true
@@ -15,4 +30,31 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-export default mongoose.model("users", UserSchema);
+UserSchema.pre<IUserDocument>("save", function(next): any {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.hash(
+    this.password,
+    10,
+    (err, hash): void => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    }
+  );
+});
+
+UserSchema.methods.comparePassword = async function comparePassword(
+  candidatePassword
+) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+export const User: IUserModel = mongoose.model<IUser, IUserModel>(
+  "users",
+  UserSchema
+);
+
+export default User;
